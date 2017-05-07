1a04a1aa3008ce2f56d32389c88dfa2800ad217a
Krzysiek <gadomski.kr@gmail.com>
2017-05-08 01:42:38 +0200

    implement messages in receive method of actord AkkademyDb

--- src/main/scala/com/gadomski/akkadb/AkkademyDb.scala
+++ src/main/scala/com/gadomski/akkadb/AkkademyDb.scala
@@ -1,8 +1,8 @@
 package com.gadomski.akkadb
 
-import akka.actor.Actor
+import akka.actor.{Actor, Status}
 import akka.event.Logging
-import com.gadomski.akkadb.messages.{SetRequest, messages}
+import com.gadomski.akkadb.messages.{GetRequest, KeyNotFoundException, SetRequest, messages}
 
 import scala.collection.mutable.HashMap
 
@@ -14,10 +14,19 @@ class AkkademyDb extends Actor {
   val log = Logging(context.system, this)
 
   override def receive: Receive = {
-    case SetRequest(key, value) => {
+    case SetRequest(key, value) =>
       log.info("received SetRequest [ key: {}, value: {}]", key, value)
       map.put(key, value)
-    }
-    case o => log.info("received unknown message: {}", o)
+      sender() ! Status.Success
+    case GetRequest(key) =>
+      log.info("received GetRequest [key: {}]", key)
+      val response: Option[Object] = map.get(key)
+      response match {
+        case Some(x) => sender() ! x
+        case None => sender() ! Status.Failure(new KeyNotFoundException(key))
+      }
+    case o =>
+      log.info("received unknown message: {}", o)
+      Status.Failure(new ClassNotFoundException)
   }
 }
